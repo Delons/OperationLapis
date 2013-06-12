dfc0ab9af12c5489b279803b49a6ae3bf00ffc36
Delons <delonsiv@gmail.com>
2013-06-12 05:51:34 -0400

    Started New Things

---
+++ src/game/Missions.java
@@ -0,0 +1,32 @@
+package game;
+
+public class Missions {
+
+	private static Missions missions;
+
+	public static Missions getInstance() {
+		if (missions == null) {
+			missions = new Missions();
+		}
+		return missions;
+	}
+	
+	String missionName;
+	
+	public void standBy(){
+		
+	}
+	
+	public void mission110(){
+		
+		missionName = "Mission 1.1.0 - Training Mission";
+		
+	}
+	
+	public void mission11a(){
+		
+		
+		
+	}
+	
+}
